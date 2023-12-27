43e42e1ccf8796b6c7b6c0a6a077121b92d7eb16
ryosuke-hiwasa <ryosuke.hiwasa@fox-hound.tech>
2023-12-27 16:17:57 +0900

    12/27質問用プッシュ
    小計再計算時、一番上の項目しか再計算が行われない
    別の商品の増減ボタンを押しても一番上の商品の再計算が行われる

--- slshop-frontend/src/main/java/com/slshop/cart/CartController.java
+++ slshop-frontend/src/main/java/com/slshop/cart/CartController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.slshop.common.entity.CartItem;
 import com.slshop.common.entity.product.Product;
@@ -45,15 +43,6 @@ public class CartController {
 	@PostMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
 		cartService.delete(id);
-		return "redirect:/cart";
-	}
-
-	@PostMapping("/update/{id}")
-	public String update(@AuthenticationPrincipal CustomerUserDetails userDetails, @PathVariable("id") Long productId,
-			@RequestParam("quantity") Integer cartQuantity, RedirectAttributes ra) {
-
-
-
 		return "redirect:/cart";
 	}
 
